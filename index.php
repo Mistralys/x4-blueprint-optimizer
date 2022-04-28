9a84e780ccf58ffd05a2ea2a90bcd6840b4548d8
Mistralys <s.mordziol@gmail.com>
2022-04-28 23:07:20 +0200

    Updated UI creation.

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
 declare(strict_types=1);
 
 use Mistralys\X4\BlueprintOptimizer;
-use Mistralys\X4\UI\UserInterface;
 
 require_once __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/config.php';
 
-$ui = new UserInterface(new BlueprintOptimizer(X4_BLUEPRINTS_PATH));
-$ui->display();
+$app = new BlueprintOptimizer(X4_BLUEPRINTS_PATH);
+$app->createUI(X4_APP_URL)->display();
+
