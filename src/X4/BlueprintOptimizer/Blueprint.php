43fb9efefce82a82f6f0e9704480596515cdb778
Mistralys <s.mordziol@gmail.com>
2022-04-28 23:08:06 +0200

    Added getter methods for modules.

--- src/X4/BlueprintOptimizer/Blueprint.php
+++ src/X4/BlueprintOptimizer/Blueprint.php
@@ -5,15 +5,30 @@ declare(strict_types=1);
 namespace Mistralys\X4\BlueprintOptimizer;
 
 use AppUtils\FileHelper;
+use Mistralys\X4\BlueprintOptimizer\Blueprint\Module;
+use Mistralys\X4\BlueprintOptimizer\Pages\EditBlueprint;
 
 class Blueprint
 {
+    public const REQUEST_PARAM_BLUEPRINT_ID = 'blueprint_id';
+
     private string $xmlFile;
     private bool $parsed = false;
+    private Collection $collection;
+
+    /**
+     * @var array<string,string>
+     */
     private array $data;
 
-    public function __construct(string $xmlFile)
+    /**
+     * @var Module[]
+     */
+    private array $modules = array();
+
+    public function __construct(Collection $collection, string $xmlFile)
     {
+        $this->collection = $collection;
         $this->xmlFile = $xmlFile;
     }
 
@@ -29,14 +44,25 @@ class Blueprint
 
     public function getURLEdit(array $params=array()) : string
     {
-        return 'https://yoyo.com';
+        $page = $this->collection->getApp()->getUI()->createPage(EditBlueprint::URL_NAME);
+
+        $params[self::REQUEST_PARAM_BLUEPRINT_ID] = $this->getID();
+
+        return $page->getURL($params);
+    }
+
+    public function getID() : string
+    {
+        $this->parse();
+
+        return $this->data['id'];
     }
 
     public function getLabel() : string
     {
         $this->parse();
 
-        return $this->data['label'];
+        return $this->data['name'];
     }
 
     private function parse() : void
@@ -49,6 +75,31 @@ class Blueprint
         $this->parsed = true;
 
         $parser = new BlueprintParser($this);
-        $this->data = $parser->getData();
+        $data = $parser->getData();
+
+        $this->data = $data['plan']['@attributes'];
+
+        if(!isset($data['plan']['entry']))
+        {
+            return;
+        }
+
+        foreach($data['plan']['entry'] as $moduleData)
+        {
+            $this->modules[] = new Module($this, $moduleData);
+        }
+    }
+
+    /**
+     * @return Module[]
+     */
+    public function getModules() : array
+    {
+        return $this->modules;
+    }
+
+    public function countModules() : int
+    {
+        return count($this->modules);
     }
 }
