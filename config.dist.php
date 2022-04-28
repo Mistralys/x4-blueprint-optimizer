d63a3e0f8fe8512db3d1ce3eab1b6a15ee891b6f
Mistralys <s.mordziol@gmail.com>
2022-04-28 23:07:07 +0200

    Added the app URL.

--- config.dist.php
+++ config.dist.php
@@ -4,4 +4,9 @@
  * @subpackage Configuration
  */
 
+// Absolute path on disk to the folder where exported
+// blueprints are stored.
 const X4_BLUEPRINTS_PATH = 'C:\Documents\Egosoft\X4\70814229\constructionplan';
+
+// Absolute URL to the application in the webserver.
+const X4_APP_URL = 'http://localhost/tools/x4-blueprint-optimizer';
