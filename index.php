b01f8ddafbd1ae171903592f98212c6ee605980e
Mistralys <s.mordziol@gmail.com>
2024-05-02 20:05:19 +0200

    Added a try/catch block.

--- index.php
+++ index.php
@@ -7,10 +7,16 @@
 declare(strict_types=1);
 
 use Mistralys\X4\BlueprintOptimizer;
+use Mistralys\X4\UI\UserInterface;
 
 require_once __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/config.php';
 
-$app = new BlueprintOptimizer(X4_BLUEPRINTS_PATH);
-$app->createUI(X4_APP_URL)->display();
-
+try {
+    $app = new BlueprintOptimizer(X4_BLUEPRINTS_PATH);
+    $app->createUI(X4_APP_URL)->display();
+}
+catch (\AppUtils\BaseException $e)
+{
+    UserInterface::displayException($e);
+}
